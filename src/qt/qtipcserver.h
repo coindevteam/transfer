6245fb866d6692098ec12655d500f2ddf8af0000
dev <dev>
2014-11-24 10:22:50 +0100

    tr

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define TWCCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "TWCCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
